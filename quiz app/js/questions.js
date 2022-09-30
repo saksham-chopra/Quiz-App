aa3bee3cb75d671f15fb182ca16907d4e5ae6714
saksham-chopra <112581990+saksham-chopra@users.noreply.github.com>
2022-09-30 10:33:41 +0530

    Add files via upload

---
+++ quiz app/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which HTML tag use to declair internal CSS?",
+    answer: "Style",
+    options: [
+      "Link",
+      "Script",
+      "Style",
+      "Both a and c"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How keywords is used to check whether a given property is valid or not?",
+    answer: "in",
+    options: [
+      "is in",
+      "in",
+      "exist",
+      "nonet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How Can we write comments in CSS?",
+    answer: "/* */",
+    options: [
+      "@",
+      "8",
+      "//",
+      "/* */"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
